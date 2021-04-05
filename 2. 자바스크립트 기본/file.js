81ea6690ee2469c1b41f8f71479715073307f321
Jungin <jungin3486@gmail.com>
2021-04-05 23:32:24 +0900

    20210405

---
+++ 2. 자바스크립트 기본/file.js
@@ -0,0 +1,39 @@
+// let message;
+
+// // message = "Hello";
+// let user = "John", //변할 수 있는 변수
+//   age = 25,
+//   message = "Hello";
+// var test = "test"; //오래된 변수 키워드, 잘 사용하지 않음
+// const myBirthday = "18.04.1982"; //변하지 않는 변수
+
+// alert(message);
+
+// let admin;
+// let name = "John";
+// admin = name;
+// alert(admin);
+
+// const birthday = "18.04.1982";
+
+// const age = someCode(birthday);
+// const bigInt = 1234567890123456789012345678901234567890n;
+
+// //문자형
+// let str = "Hello";
+// let phrase = `can embed another ${str}`; //문자열은 따옴표로 묶기
+
+// let name = "John";
+
+// // 변수를 문자열 중간에 삽입
+// // alert(`Hello, ${name}!`); // Hello, John!
+
+// //불린형
+// let nameFieldChecked = true; // 네, name field가 확인되었습니다(checked).
+// let ageFieldChecked = false; // 아니요, age field를 확인하지 않았습니다(not checked)
+
+//객체와 심볼
+let question = prompt("당신의 이름은 무엇인가요?", "이름입력");
+// let age = prompt("나이를 입력해주세요.", 100);
+
+alert(`당신의 이름은 ${question} 입니다.`); // 당신의 나이는 100살입니다.
